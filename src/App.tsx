b5938795501c674f623fdc8d0a69a89adfa79390
LQTjim <jim6050482@hotmail.com>
2023-09-14 14:18:31 +0800

    ADD:background UPDATE:refactor Demo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,19 @@
 import { Link } from "react-router-dom";
 import "./App.css";
-import * as HoverCard from "@radix-ui/react-hover-card";
-import { Flex, Box, Heading, Text, Avatar } from "@radix-ui/themes";
+
+import { Box } from "@radix-ui/themes";
 import { patheConfigs } from "./config/pathConfig";
 import type { PathConfig } from "./config/pathConfig";
+import Demo from "./components/Demo";
 
 function App() {
   return (
-    <Box className="flex w-full flex-col items-center justify-center text-lg">
-      <Box className="pb-1 ">
+    <Box className="flex w-full flex-col items-center justify-center text-lg text-slate-50">
+      <Box className="pb-1">
         下方連結導向對應githubpages，hover標題可以預覽縮圖
       </Box>
       {patheConfigs.map((pathConfig: PathConfig) => {
-        const { name, url, imgSrc, description } = pathConfig;
-        return (
-          <Box className="bg-gray-50 py-4 px-5 mb-1 mx-2 w-1/2">
-            <HoverCard.Root>
-              <HoverCard.Trigger>
-                <Link
-                  className="cursor-pointer text-red-500"
-                  to={url}
-                  target="_blank"
-                >
-                  {name}
-                </Link>
-              </HoverCard.Trigger>
-              <HoverCard.Content className="bg-lime-50 rounded-md shadow-md">
-                <Box className="flex flex-col gap-5">
-                  <img src={imgSrc} className="w-[450px]" />
-                  <Box className="p-4">
-                    <Box className="text-2xl font-black mb-1">{name}</Box>
-                    <Text as="div" size="2" style={{ maxWidth: 300 }} mt="3">
-                      {description}
-                    </Text>
-                  </Box>
-                </Box>
-              </HoverCard.Content>
-            </HoverCard.Root>{" "}
-          </Box>
-        );
+        return <Demo key={pathConfig.name} {...pathConfig} />;
       })}
       <Box className="py-1">
         *切版素材來自 :
@@ -50,7 +25,7 @@ function App() {
           >
             frontend-mentor
           </Link>
-         ，
+          ，
           <Link
             className="text-red-500"
             to="https://dq.yam.com/"
---
+++ src/components/Demo.tsx
@@ -0,0 +1,32 @@
+import * as HoverCard from "@radix-ui/react-hover-card";
+import { Box, Text } from "@radix-ui/themes";
+import { Link } from "react-router-dom";
+import type { PathConfig } from "../config/pathConfig";
+const Demo = ({ url, name, imgSrc, description }: PathConfig) => {
+  return (
+    <Box className="bg-gray-50 py-4 px-5 mb-2 mx-2 w-1/2 rounded-sm ">
+      <HoverCard.Root>
+        <HoverCard.Trigger>
+          <Link
+            className="cursor-pointer text-red-500"
+            to={url}
+            target="_blank"
+          >
+            {name}
+          </Link>
+        </HoverCard.Trigger>
+        <HoverCard.Content className="bg-lime-50 rounded-md shadow-md">
+          <Box className="flex flex-col gap-5 text-black">
+            <img src={imgSrc} className="w-[450px]" />
+            <Box className="p-4">
+              <Box className="text-2xl font-black mb-1">{name}</Box>
+              <Text>{description}</Text>
+            </Box>
+          </Box>
+        </HoverCard.Content>
+      </HoverCard.Root>{" "}
+    </Box>
+  );
+};
+
+export default Demo;
