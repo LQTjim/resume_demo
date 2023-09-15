26c0a76e7888bd1a29aec3c3e12c020754188575
LQTjim <jim6050482@hotmail.com>
2023-09-15 17:09:35 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 
-export default ({ mode }) => {
-  const VITE_BASENAME: string = loadEnv(mode, process.cwd()).VITE_BASENAME;
-
+export default () => {
   return defineConfig({
     plugins: [react()],
-    base: VITE_BASENAME,
+    base: "resume_demo",
   });
 };
