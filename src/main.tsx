71ea2ff8a0a982fb5d1539d8533d8700e826628d
LQTjim <jim6050482@hotmail.com>
2023-09-15 17:11:49 +0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import "@radix-ui/themes/styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename={`${import.meta.env.VITE_BASENAME}`}>
+    <BrowserRouter basename="resume_demo">
       <App />
     </BrowserRouter>
   </React.StrictMode>
